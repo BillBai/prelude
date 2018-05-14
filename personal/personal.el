71d8879fbcf236ef75efad2969b6b3b9a89a5eba
billbai <baiyinfeng94@gmail.com>
2018-05-14 18:08:42 +0800

    add irony company config

--- personal/personal.el
+++ personal/personal.el
@@ -122,6 +122,14 @@
 (prelude-require-package 'yasnippet)
 (prelude-require-package 'auto-complete-c-headers)
 
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+;; (optional) adds CC special commands to `company-begin-commands' in order to
+;; trigger completion at interesting places, such as after scope operator
+;;     std::|
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
 (yas-global-mode 1)
 
 
