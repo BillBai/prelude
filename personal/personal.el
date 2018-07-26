7f3117e5cc8aaeeb09d529341fab55ee13a4f112
billbai(白银峰) <billbai@tencent.com>
2018-07-26 12:34:40 +0800

    clean up js config

--- personal/personal.el
+++ personal/personal.el
@@ -36,9 +36,10 @@
 
 (cnfonts-enable)
 
-(add-to-list 'default-frame-alist '(height . 42))
-(add-to-list 'default-frame-alist '(width . 123))
+(add-to-list 'default-frame-alist '(height . 67))
+(add-to-list 'default-frame-alist '(width . 120))
 
+(prelude-require-package 'solarized-theme)
 (prelude-require-package 'spacemacs-theme)
 (prelude-require-package 'material-theme)
 
@@ -98,33 +99,32 @@
   ;; 1. 光标只有在注释里面时，才可以输入中文。
   ;; 2. 光标前是汉字字符时，才能输入中文。
   ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-  ;; (setq-default pyim-english-input-switch-functions
-  ;;               '(pyim-probe-dynamic-english
-  ;;                 pyim-probe-isearch-mode
-  ;;                 pyim-probe-program-mode
-  ;;                 pyim-probe-org-structure-template))
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
 
   (setq-default pyim-punctuation-half-width-functions
                 '(pyim-probe-punctuation-line-beginning
                   pyim-probe-punctuation-after-punctuation))
 
   ;; 开启拼音搜索功能
-  (pyim-isearch-mode 1)
+  ;; (pyim-isearch-mode 1)
 
   ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
   ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
   ;; 手动安装 posframe 包。
   (setq pyim-page-tooltip 'posframe)
 
-  ;; 选词框显示5个候选词
+  ;; 选词框显示7个候选词
   (setq pyim-page-length 7)
 
   ;; 让 Emacs 启动时自动加载 pyim 词库
   (add-hook 'emacs-startup-hook
             #'(lambda () (pyim-restart-1 t)))
   :bind
-  (
-   ;; ("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
+  (("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
    ("C-;" . pyim-delete-word-from-personal-buffer)))
 
 
@@ -261,7 +261,6 @@
 
 (prelude-require-package 'js2-mode)
 (prelude-require-package 'js2-refactor)
-(prelude-require-package 'xref-js2)
 (prelude-require-package 'json-mode)
 
 
@@ -273,26 +272,11 @@
 (js2r-add-keybindings-with-prefix "C-c C-r")
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
 
-;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-;; unbind it.
-(define-key js-mode-map (kbd "M-.") nil)
-
-(add-hook 'js2-mode-hook (lambda ()
-                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 (prelude-require-package 'web-beautify)
 
 (eval-after-load 'js2-mode
   '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-
-
-(prelude-require-package 'company-tern)
-
-(add-to-list 'company-backends 'company-tern)
-(add-hook 'js2-mode-hook (lambda ()
-                           (tern-mode)
-                           (company-mode)))
-
 
 
 ;;;
