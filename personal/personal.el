f5bbd4ac22ca6e072af2f40126e9753452e9b636
billbai(白银峰) <billbai@tencent.com>
2018-08-21 15:14:42 +0800

    add make self header func

--- personal/personal.el
+++ personal/personal.el
@@ -9,11 +9,24 @@
   (interactive)
   (insert "/**" "\n")
   (insert "* File: " (file-name-nondirectory buffer-file-name) "\n")
-  (insert "* Project: " "melo" "\n")
+  (insert "* Project: " (projectile-project-name) "\n")
   (insert "* Created Date: " (format-time-string "%A, %B %d %Y, %l:%M:%S %p") "\n")
   (insert "* Author: billbai billbai@tencent.com" "\n")
   (insert "* -----" "\n")
   (insert "* Copyright (c) " (format-time-string "%Y") " Tencent" "\n")
+  (insert "*/" "\n"))
+
+
+(defun make-self-file-header ()
+  "Insert self project header comment for file"
+  (interactive)
+  (insert "/**" "\n")
+  (insert "* File: " (file-name-nondirectory buffer-file-name) "\n")
+  (insert "* Project: " (projectile-project-name) "\n")
+  (insert "* Created Date: " (format-time-string "%A, %B %d %Y, %l:%M:%S %p") "\n")
+  (insert "* Author: billbai billbai42@gmail.com" "\n")
+  (insert "* -----" "\n")
+  (insert "* Copyright (c) " (format-time-string "%Y") " billbai" "\n")
   (insert "*/" "\n"))
 
 ;;
