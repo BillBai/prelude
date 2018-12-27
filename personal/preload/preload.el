c4cfd61ac1af5371c0f3d00b2dc59e2315c35e9f
billbai(白银峰) <billbai@tencent.com>
2018-12-27 11:44:08 +0800

    switch spacemacs theme

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,5 +1,5 @@
 ;; (scroll-bar-mode -1)
-;; (setq url-proxy-services
-;;       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-;;         ("http" . "web-proxy.tencent.com:8080")
-;;         ("https" . "web-proxy.tencent.com:8080")))
+ (setq url-proxy-services
+       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+         ("http" . "web-proxy.tencent.com:8080")
+         ("https" . "web-proxy.tencent.com:8080")))
