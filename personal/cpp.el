ec8a6d205b2a157bdeb5563fbabd0c705dd16d8c
billbai(白银峰) <billbai@tencent.com>
2019-10-09 12:00:56 +0800

    back to ccls. add pyim-bigdict

--- personal/cpp.el
+++ personal/cpp.el
@@ -5,6 +5,7 @@
 (prelude-require-packages '(clang-format
                             google-c-style                            
                             lsp-mode
+                            ccls
                             dap-mode
                             lsp-treemacs
                             lsp-ui
@@ -37,8 +38,6 @@
          (c-mode . lsp-deferred))
   :commands (lsp lsp-deferred)
   :config
-  ;; `-background-index' requires clangd v8+!
-  (setq lsp-clients-clangd-args '("-j=6" "-background-index" "-log=error"))
   ;; Prefer using lsp-ui (flycheck) over flymake.
   (setq lsp-prefer-flymake nil) )
 
@@ -72,63 +71,63 @@
 
 (use-package dap-lldb)
 
-;; (use-package ccls
-;;   :hook
-;;   ((c-mode c++-mode objc-mode) .
-;;    (lambda () (require 'ccls) (lsp)))
-;;   :config
-;;   (setq ccls-executable (file-truename "~/Developer/ccls/Release/ccls"))
-;;   (setq ccls-sem-highlight-method 'font-lock)
-;;   (setq lsp-prefer-flymake nil)
-;;   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+(use-package ccls
+  :hook
+  ((c-mode c++-mode objc-mode) .
+   (lambda () (require 'ccls) (lsp)))
+  :config
+  (setq ccls-executable (file-truename "~/Developer/ccls/Release/ccls"))
+  (setq ccls-sem-highlight-method 'font-lock)
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
   
-;;   ;; https://github.com/MaskRay/Config/blob/master/home/.config/doom/modules/private/my-cc/autoload.el#L10
-;;   (defun ccls/callee ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
-;;   (defun ccls/caller ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "$ccls/call"))
-;;   (defun ccls/vars (kind)
-;;     (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
-;;   (defun ccls/base (levels)
-;;     (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
-;;   (defun ccls/derived (levels)
-;;     (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
-;;   (defun ccls/member (kind)
-;;     (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
+  ;; https://github.com/MaskRay/Config/blob/master/home/.config/doom/modules/private/my-cc/autoload.el#L10
+  (defun ccls/callee ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+  (defun ccls/caller ()
+    (interactive)
+    (lsp-ui-peek-find-custom "$ccls/call"))
+  (defun ccls/vars (kind)
+    (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
+  (defun ccls/base (levels)
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
+  (defun ccls/derived (levels)
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
+  (defun ccls/member (kind)
+    (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
 
-;;   ;; The meaning of :role corresponds to https://github.com/maskray/ccls/blob/master/src/symbol.h
-;;   ;; References w/ Role::Address bit (e.g. variables explicitly being taken addresses)
-;;   (defun ccls/references-address ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "textDocument/references"
-;;                              (plist-put (lsp--text-document-position-params) :role 128)))
+  ;; The meaning of :role corresponds to https://github.com/maskray/ccls/blob/master/src/symbol.h
+  ;; References w/ Role::Address bit (e.g. variables explicitly being taken addresses)
+  (defun ccls/references-address ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 128)))
 
-;;   ;; References w/ Role::Dynamic bit (macro expansions)
-;;   (defun ccls/references-macro ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "textDocument/references"
-;;                              (plist-put (lsp--text-document-position-params) :role 64)))
+  ;; References w/ Role::Dynamic bit (macro expansions)
+  (defun ccls/references-macro ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 64)))
 
-;;   ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
-;;   (defun ccls/references-not-call ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "textDocument/references"
-;;                              (plist-put (lsp--text-document-position-params) :excludeRole 32)))
+  ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+  (defun ccls/references-not-call ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :excludeRole 32)))
 
-;;   ;; References w/ Role::Read
-;;   (defun ccls/references-read ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "textDocument/references"
-;;                              (plist-put (lsp--text-document-position-params) :role 8)))
+  ;; References w/ Role::Read
+  (defun ccls/references-read ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 8)))
 
-;;   ;; References w/ Role::Write
-;;   (defun ccls/references-write ()
-;;     (interactive)
-;;     (lsp-ui-peek-find-custom "textDocument/references"
-;;                              (plist-put (lsp--text-document-position-params) :role 16)))
-;;   )
+  ;; References w/ Role::Write
+  (defun ccls/references-write ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 16)))
+  )
 
 (provide 'cpp)
 ;; cpp.el ends here
--- personal/pyim.el
+++ personal/pyim.el
@@ -2,13 +2,11 @@
 ;;   Chinese Input Method
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(pyim
-                            quelpa
+(prelude-require-packages '(pyim                            
                             pyim-basedict
                             posframe))
 
 (use-package pyim
-  ;; :after liberime-config
   :config
   (setq default-input-method "pyim")
   
@@ -21,8 +19,23 @@
   (setq pyim-page-tooltip 'posframe)
 
   ;; 选词框显示 5 个候选词
-  ;; (setq pyim-page-length 5)
+  (setq pyim-page-length 5)
 
+  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+  ;; 我自己使用的中英文动态切换规则是：
+  ;; 1. 光标只有在注释里面时，才可以输入中文。
+  ;; 2. 光标前是汉字字符时，才能输入中文。
+  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+  
   ;; 激活 basedict 拼音词库
   (use-package pyim-basedict
     :ensure nil
@@ -30,12 +43,23 @@
   
   ;; 我使用全拼
   (setq pyim-default-scheme 'quanpin)
-
-  ;; (setq pyim-default-scheme 'rime)
   
   ;; use western punctuation
   (setq pyim-punctuation-dict nil)
+
+  (setq pyim-dicts
+        '((:name "bigdict" :file (file-truename "~/.emacs/pyim-bigdict.pyim"))
+          ))
+
+  :bind
+  ;与 pyim-probe-dynamic-english 配合
+  (("M-J" . pyim-convert-string-at-point))
   )
 
+(setq default-input-method "pyim")
+
+(global-set-key (kbd "C-\\") 'toggle-input-method)
+
 (provide 'pyim)
+
 ;; pyim.el ends here
